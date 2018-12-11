49d7050267c4d9089ce11f70362ee474047ea244
Alexandros Georgakopoulos <44508158+alexgeor9@users.noreply.github.com>
2018-12-11 17:04:31 -0500

    Add files via upload

--- VM.py
+++ VM.py
@@ -1,6 +1,6 @@
 '''
 Author: Alexandros Georgakopoulos
-Class: EECS545 Machine Learning
+Class: EECS 545 Machine Learning
 Title: VM Implementation
 Date: 12-08-2018
 '''
@@ -34,83 +34,78 @@ L = graph.laplacian
 #import the graph here, only the Laplacian L is needed
 #n=50 #n=|V|, numbers of nodes of the graph
 #l=10 #l=budget of nodes we want to learn their true label
-#import random
-#L_r=np.random.rand(n,n) #example
-#L=(L_r + L_r.T)/2 #example
+# import random
+# L_r=np.random.rand(n,n) #example
+# L=(L_r + L_r.T)/2 #example
 
 #eigendecomposition with decreasing eigenvalues
 def sorteigen(L):
     eigenValues, eigenVectors = np.linalg.eig(L)
-    idx = np.argsort(eigenValues)
+    idx = eigenValues.argsort()[::-1][:]
     eigenValues = eigenValues[idx]
     eigenVectors = eigenVectors[:,idx]
     return (eigenValues, eigenVectors)
 
 #using L we define the following quantities used throughout the algorithm
-X=sorteigen(L)[1] #L=XSigmaX^T, is it X_TSigmaX?
+X=sorteigen(L)[1] #L=XSigmaX^T
 Sigma=np.diag(sorteigen(L)[0]) #the diagonal entries must decrease and the nxn entry must be zero
 M=Sigma+np.identity(n) #M=Sigma+I
 M_inv=np.linalg.inv(M) #used in equations (12) and (20)
-X_cal=[(j,X[j,:]) for j in range(n)] #X_cal is a list of (j, j-th row of X)
-K=[] #list to be filled with l tuples (i,p), i nodes to be predicted, p= ith row of X
+K=[]
 def f(x):
-    return -(x+1)/x
+    return -(x+1.0)/x
 B_hat_inv=np.zeros((n,n)) #used in equations (12) and (15)
-B_hat_inv[:n-1,:n-1]=np.diag(f(sorteigen(L)[0][:n-1])) #puts -(ev+1)/ev on the diagonal from 1 to n-1
+B_hat_inv[:n-1,:n-1]=np.diag(f(sorteigen(L)[0][:n-1])) #puts -(eigenvalue+1)/eigenvalue on the diagonal from 1 to n-1
 A_0=M_inv-np.identity(n) #M^{-1}-I, used in equations (12) and (16)
 
-#Finding the first point to label, equation (12)
+#four auxiliary functions
 def C(p,B): #quantity in right hand side of equation (15), used in equation (12)
     v_hat=np.append(p[:n-1], 0)
-    den=1+np.dot(np.dot(v_hat.T,B),v_hat)
-    return B-(1.0/den)*np.dot(np.dot(np.dot(v_hat.T,B),v_hat.T),B)
-
-ind,p_1=X_cal[0]
-max=np.trace(np.dot(C(p_1,B_hat_inv),M_inv)) #equation (12)
-for m,p in X_cal[1:]: #this loop solves the argmax problem of equation (12)
-    tr=np.trace(np.dot(C(p,B_hat_inv),M_inv))
-    if max > tr:
-        max=tr
-        ind,p_1=m,p
-K.append((ind,p_1))
-
-#Recursively finding the next l-1 points to label
+    denominator=1.0+np.dot(np.dot(v_hat.T,B),v_hat)
+    return B-(1.0/denominator)*np.dot(np.dot(np.dot(B,v_hat),v_hat.T),B)
 def D(p,A): #quantity in right hand side of equation (18), used in equation (20) for j>1
-    den=1+np.dot(np.dot(p.T,A),p)
-    return A-(1.0/den)*np.dot(np.dot(np.dot(A,p),p.T),A)
+    denominator=1.0+np.dot(np.dot(p.T,A),p)
+    return A-(1.0/denominator)*np.dot(np.dot(np.dot(A,p),p.T),A)
+def quantity_20(A,t):
+    return (1.0/1.0+np.dot(np.dot(X[t,:].T,A),X[t,:]))*np.dot(np.dot(np.dot(np.dot(X[t,:].T,A),M_inv),A),X[t,:])
+def argmin_outside_K(A,K):
+    min_index=list(set(range(n)) - set(K))[0] #initialisation for the loop below
+    min_quantity=quantity_20(A,min_index)
+    for t in range(n): #here using a loop for finding argmin
+        if t not in K and min_quantity < quantity_20(A,t):
+            min_index=t
+            min_quantity=quantity_20(A,t)
+    return min_index
 
-for j in range(1,l): #this loop recursively finds all the nodes of L_cal after the first
+#Finding the first point to label, equation (12)
+#this solves the argmax problem of equation (12)
+ind=np.argmax([np.trace(np.dot(C(X[j,:],B_hat_inv),M_inv)) for j in range(n)])
+p_1=X[ind,:]
+K.append(ind)
+
+#Recursively finding the next l-1 points of L_cal to label
+for j in range(1,l):
     if j==1:
         A_j_inv=C(p_1,B_hat_inv) #for j=1 we use equation (15)
     else:
         A_j_inv=D(new_p,A_j_inv) #for j>1 we use equation (18)
-    #the next three lines initialise the loop that solves the optimization problem of equation (20)
-    index,new_p=X_cal[0]
-    den_2=1+np.dot(np.dot(new_p.T,A_j_inv),new_p) #equation (20)
-    min=(1.0/den_2)*np.dot(np.dot(np.dot(np.dot(new_p.T,A_j_inv),M_inv),A_j_inv),new_p) #equation (20)
-    for m,p in X_cal[1:]:  #this loop solves the argmin problem of equation (20)
-        if (m,p) not in K:
-            den_2=1+np.dot(np.dot(p.T,A_j_inv),p) #equation (20)
-            frac=(1.0/den_2)*np.dot(np.dot(np.dot(np.dot(p.T,A_j_inv),M_inv),A_j_inv),p) #equation (20)
-            if min <= frac:
-                min= frac
-                index,new_p=m,p
-    K.append((index,new_p))
+    index=argmin_outside_K(A_j_inv,K)
+    new_p=X[index,:]
+    K.append(index)
 
-L_cal=np.sort([K[j][0] for j in range(l)]) #the indices in K, K must have length l here
+L_cal=K
+print(L_cal)
 
-print("L_cal = ", L_cal)
+# import code
+# code.interact(local=locals())
 
 for i in L_cal:
-    graph.set_label(i, labels[i])
+   graph.set_label(i, labels[i])
 
-#for i in range(l):
-#    graph.set_label(i, labels[i])
+for i in range(l):
+   graph.set_label(i, labels[i])
 
 LP(graph)
 
 train_error = (graph.labels == labels).sum() / labels.size
 print("Training error using VOpt+LP = ", train_error)
-
-#import code
-#code.interact(local=locals())
